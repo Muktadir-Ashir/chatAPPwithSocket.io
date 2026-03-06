69bdc163d6e711a92d0aa15cb73ddfedb27faab2
Muktadir-Ashir <ashirahmed04115@gmail.com>
2026-03-06 17:39:22 +0600

    feat: add CORS support, axios instance, and authentication store; update routing logic in App component

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 
 
 import { connectDB } from "./lib/db.js";
@@ -17,6 +18,10 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
