61c7073156c84c0bc634b7ec1d5ee495f152bf5e
Muktadir-Ashir <ashirahmed04115@gmail.com>
2026-03-12 04:25:17 +0600

    Socket.io Implementation

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -43,6 +43,22 @@ export const useChatStore = create((set, get) => ({
         }
     },
 
+    subscribeToMessages: () => {
+        const { selectedUser } = get();
+        if (!selectedUser) return;
+
+        const socket = useAuthStore.getState().socket;
+
+        socket.on("newMessage", (newMessage) => {
+            const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
+            if (!isMessageSentFromSelectedUser) return;
+
+            set({
+                messages: [...get().messages, newMessage],
+            });
+        });
+    },
+
     //todo: optimize this one later
     setSelectedUser: (selectedUser) => set({ selectedUser }),
 }));
